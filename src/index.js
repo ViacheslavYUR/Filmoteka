f7cc5d7f86823c8dc3cc7e6289e13d2632c58e70
Viacheslav-Yurushev <108462275+ViacheslavYUR@users.noreply.github.com>
2022-12-31 18:12:30 +0200

    fixin

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { hideScrollUpBtn } from './js/hide-scroll-up-button';
 import { smoothscroll } from './js/smoothscroll';
 import { renderMarkup } from './js/showTrending/renderTrending';
 import { hiddenElementsOnMobileVersion } from './js/hideElementsInMobileVersion';
-import './js/pagination/pagination'
+import { handleInputSearchMovie } from './js/inputSearch/inputMovieSearch';
+import './js/pagination/pagination';
 smoothscroll();
 
 window.addEventListener('scroll', hideScrollUpBtn);
