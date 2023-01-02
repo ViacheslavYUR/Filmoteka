04954c3a577666056934a20107dd41eabc3b74b3
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-01-02 11:42:17 +0200

    Update pagination.js

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -18,11 +18,11 @@ const options = {
   visiblePages: 10,
   page: 1,
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    page: '<a href="&" class="tui-page-btn">{{page}}</a>',
     currentPage:
       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<a href="&" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
     disabledMoveButton:
@@ -30,7 +30,7 @@ const options = {
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<a href="&" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
       '<span class="tui-ico-ellip">...</span>' +
       '</a>',
   },
