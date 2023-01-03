316d4c455a867d177cc28be1f17741b3c1018993
B1lkao <hrabousolesia@gmail.com>
2023-01-03 23:54:32 +0200

    Додала addWatchedQue.js та getWatchedQueue.js

---
+++ src/js/addWatchedQue.js
@@ -0,0 +1,31 @@
+import Notiflix from 'notiflix';
+export const handleClick = (e, movieData) => {
+  const { target } = e;
+ Notiflix.Notify.success('You have added this movie to the ${e.target.textContent.slice(6).toUpperCase()} .It is in the library');
+  const type = target.textContent.includes('watched') ? 'watched' : 'queue';
+
+  prepareDataSetToLS(type, movieData);
+};
+
+const prepareDataSetToLS = (type, movie) => {
+  const storageMovies = getMoviesFromLS(type);
+  const isAlredyInStorage =
+    storageMovies &&
+    !!storageMovies.find(storageMovie => storageMovie.id === movie.id);
+
+  if (isAlredyInStorage) return;
+
+  if (!storageMovies) {
+    return setMoviesToLS(type, [movie]);
+  }
+
+  return setMoviesToLS(type, [...storageMovies, movie]);
+};
+
+const setMoviesToLS = (type, data) => {
+  localStorage.setItem(type, JSON.stringify(data));
+};
+
+const getMoviesFromLS = type => {
+  return JSON.parse(localStorage.getItem(type));
+};
---
+++ src/js/getWatchedQueue.js
@@ -0,0 +1,43 @@
+import { getAllMovieApi } from './getAllMovieApi';
+
+const libraryUl = document.querySelector('.header-nav_list');
+const libraryBack = document.querySelector('.header-nav_libr');
+const watchedBtn = document.querySelector('#header_btn-watched');
+const queueBtn = document.querySelector('#header_btn-queue');
+
+const body = document.querySelector('body');
+const backdropLibrary = document.querySelector('.backdrop');
+
+const loadToPageWatch = () => {
+  if (!JSON.parse(localStorage.getItem('watched'))) {
+    return (libraryUl.textContent = '');
+  } else return watched.map(watchCard).join('');
+};
+const loadToPageQueue = () => {
+  if (!JSON.parse(localStorage.getItem('queue'))) {
+    return (libraryUl.textContent = '');
+  } else return queue.map(queueCard).join('');
+};
+const watched = JSON.parse(localStorage.getItem('watched'));
+const queue = JSON.parse(localStorage.getItem('queue'));
+
+watchedBtn.addEventListener('click', onWatchedClick);
+queueBtn.addEventListener('click', onQueueClick);
+
+function onWatchedClick() {
+  libraryUl.innerHTML = '';
+  libraryUl.insertAdjacentHTML('beforeend', loadToPageWatch());
+  libraryUl.classList.remove('visually-hidden');
+  libraryBack.classList.add('visually-hidden');
+  watchedBtn.classList.toggle('activeBtn');
+  queueBtn.classList.remove('activeBtn');
+}
+
+function onQueueClick() {
+  libraryUl.innerHTML = '';
+  libraryUl.insertAdjacentHTML('beforeend', loadToPageQueue());
+  libraryUl.classList.remove('visually-hidden');
+  libraryBack.classList.add('visually-hidden');
+  queueBtn.classList.toggle('activeBtn');
+  watchedBtn.classList.remove('activeBtn');
+}
