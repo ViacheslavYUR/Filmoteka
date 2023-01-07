32b984f1eb356035faecd4699676f8f6c7fc0214
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-01-07 20:36:08 +0200

    update style

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -15,20 +15,22 @@ const options = {
   itemsPerPage: PER_PAGE,
   page: 1,
   visiblePages: 5,
+  centerAlign: false,
+  usageStatistics:false,
   template: {
-    page: '<a href="&" class="tui-page-btn">{{page}}</a>',
+    page: '<a href="&" class="tui-custom">{{page}}</a>',
     currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      '<span class="tui-custom tui-custom-is-selected">{{page}}</span>',
     moveButton:
-      '<a href="&" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<a href="&" class="tui-custom tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
     disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-custom tui-is-disabled tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="&" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<a href="&" class="tui-custom tui-{{type}}-is-ellip">' +
       '<span class="tui-ico-ellip">...</span>' +
       '</a>',
   },
